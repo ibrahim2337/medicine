605a6218d4fe8f9385cfc5c7eada3e50651bdfba
ibrahim2337 <ibrahimkhalilullah680@gmail.com>
2024-02-02 19:43:25 +0600

    mask feature ready

--- src/component/MaskFeature/MaskFeature.jsx
+++ src/component/MaskFeature/MaskFeature.jsx
@@ -1,11 +1,24 @@
 import SideImage from "../../assets/SingleProduct/638a77dcdd7bb-square.jpg"
-
+import maskImage from "../../assets/SingleProduct/5eb191ac13910.jpg"
 const MaskFeature = () => {
     return (
-        <div className="grid grid-cols-3">
-            <div>
+        <div className="grid grid-cols-3 gap-5 px-5 lg:px-20">
+            <div className="">
                 <img src={SideImage} alt="" />
             </div>
+            <div className="grid grid-cols-2 gap-5">
+            <img src={maskImage} alt="" />
+            <img src={maskImage} alt="" />
+            <img src={maskImage} alt="" />
+            <img src={maskImage} alt="" />
+            </div>
+            <div className="grid grid-cols-2 gap-5">
+            <img src={maskImage} alt="" />
+            <img src={maskImage} alt="" />
+            <img src={maskImage} alt="" />
+            <img src={maskImage} alt="" />
+            </div>
+            
         </div>
     );
 };
