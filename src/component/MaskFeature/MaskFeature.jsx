f9da1929f0cc5246e8e78cb434b06d2d18922215
ibrahim2337 <ibrahimkhalilullah680@gmail.com>
2024-02-02 19:45:58 +0600

    mask section mobile responsive ok

--- src/component/MaskFeature/MaskFeature.jsx
+++ src/component/MaskFeature/MaskFeature.jsx
@@ -2,7 +2,7 @@ import SideImage from "../../assets/SingleProduct/638a77dcdd7bb-square.jpg"
 import maskImage from "../../assets/SingleProduct/5eb191ac13910.jpg"
 const MaskFeature = () => {
     return (
-        <div className="grid grid-cols-3 gap-5 pb-10 px-5 lg:px-20">
+        <div className="grid lg:grid-cols-3 gap-5 pb-10 px-5 lg:px-20">
             <div className="">
                 <img src={SideImage} alt="" />
             </div>
